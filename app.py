aec2b34841aa876f71522dcacb2970b06d9634fc
mikh <mikh@rwxrwxrwx.dev>
2022-03-30 22:40:16 +0300

    add templates, css styles

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/me')
+def me():
+    return render_template('me.html', request=request)
+
+@app.route('/tools')
+def tools():
+    return render_template('tools.html', request=request)
 
 if __name__ == '__main__':
     app.run()
