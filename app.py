19bb72d435ca864c66280302342f8a4d1dfd58b3
mikh <mikh@rwxrwxrwx.dev>
2022-03-31 11:52:05 +0300

    add metrics

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, redirect, url_for, request, jsonify
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+metrics.info('app_info', 'Application info', version='1.0.3')
 
 @app.route('/')
 def index():
@@ -13,6 +17,13 @@ def me():
 @app.route('/tools')
 def tools():
     return render_template('tools.html', request=request)
+
+metrics.register_default(
+    metrics.counter(
+        'by_path_counter', 'Request count by request paths',
+        labels={'path': lambda: request.path}
+    )
+)
 
 if __name__ == '__main__':
     app.run()
