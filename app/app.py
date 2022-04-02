795564e79bb17346112731912594dfa726da937d
mikh <mikh@rwxrwxrwx.dev>
2022-04-02 21:57:08 +0300

    add health checks

--- app/app.py
+++ app/app.py
@@ -1,17 +1,60 @@
 from flask import Flask, render_template, redirect, url_for, request
 from prometheus_flask_exporter import PrometheusMetrics
+from logging.config import dictConfig
+from flask_healthz import healthz
+from flask_healthz import HealthError
 import contentful
+import time
 import os
 # import flask-markdown2
 # import markdown2
 
-app = Flask(__name__)
-metrics = PrometheusMetrics(app)
-
-metrics.info('app_info', 'Application info', version='1.0.3')
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 CONTENTFUL_SPACE_ID = os.environ['CONTENTFUL_SPACE_ID']
 CONTENTFUL_ACEESS_TOKEN = os.environ['CONTENTFUL_ACEESS_TOKEN']
+
+app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+app.register_blueprint(healthz, url_prefix="/healthz")
+metrics.info('app_info', 'Application info', version='1.0.3')
+
+def printok():
+    print("Everything is ok")
+
+def liveness():
+    try:
+        printok()
+    except Exception:
+        raise HealthError("Not ready")
+
+def readiness():
+    try:
+        printok()
+    except Exception:
+        raise HealthError("Not ready")
+
+app.config.update(
+    HEALTHZ = {
+        "live": "app.liveness",
+        "ready": "app.readiness",
+    }
+)
 
 @app.route('/')
 def index():
@@ -41,6 +84,7 @@ def tools():
     entry = client.entry(CONTENTFUL_TOOLS_ENTRY_ID)
     content = entry.content
     ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    app.logger.info('ip: ' + ip)
 
     return render_template('tools.html', ip=ip, content=content)
 
